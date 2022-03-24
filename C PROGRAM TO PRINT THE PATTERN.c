cc3f5a2e506f10a06bbbe5cede9e9ca399621a6f
NIVEDHA-V <101490704+NIVEDHA-V@users.noreply.github.com>
2022-03-24 22:47:56 +0530

    Create C PROGRAM TO PRINT THE PATTERN.c
    Using C , we can display various patterns. One such pattern is provided above as comment. It is displayed using nested for loop concept.

---
+++ C PROGRAM TO PRINT THE PATTERN.c
@@ -0,0 +1,35 @@
+//PROGRAM TO PRINT FOLLOWING PATTERN
+
+/*
+^
+^^
+^^^
+^^^^
+^^^^^
+^^^^
+^^^
+^^
+^
+*/
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("^");
+		}
+		printf("\n");
+	}
+	for(i=4;i>=1;i--)
+	{
+		for(j=i;j>=1;j--)
+		{
+			printf("^");
+		}
+		printf("\n");
+	}
+	return 0;
+}
